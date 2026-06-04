066e31d161f35f86008bad3e02004814dafbd7df
Void491111 <voidw87@gmail.com>
2026-06-04 11:06:34 +0700

    fix navbar yang nempel dengan keyboard saat keyboard muncul

--- lib/features/home/views/home_page.dart
+++ lib/features/home/views/home_page.dart
@@ -57,6 +57,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         bottom: false,
         child: ListenableBuilder(
