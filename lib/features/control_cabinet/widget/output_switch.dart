99c65273b8229667340631361ec01d5805f6f266
Void491111 <voidw87@gmail.com>
2026-06-04 13:43:29 +0700

    membuat ui untuk fitur output_switch pada fitur cabinet_control

--- lib/features/control_cabinet/widget/output_switch.dart
+++ lib/features/control_cabinet/widget/output_switch.dart
@@ -1,9 +1,41 @@
-class IOModel {
-  String label;
-  bool isOn;
+// lib/features/control_cabinet/widgets/output_switch.dart
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-  IOModel({
+class OutputSwitch extends StatelessWidget {
+  final String label;
+  final bool isOn;
+  final ValueChanged<bool> onChanged;
+
+  const OutputSwitch({
+    super.key,
     required this.label,
-    this.isOn = false,
+    required this.isOn,
+    required this.onChanged,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Transform.scale(
+          scale: 0.8, // Dikecilin dikit biar muat 5 kolom dengan rapi
+          child: Switch(
+            value: isOn,
+            onChanged: onChanged,
+            activeColor: Colors.white,
+            activeTrackColor: const Color(0xFFD32F2F),
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey[300],
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          label,
+          style: GoogleFonts.poppins(fontSize: 10, color: Colors.grey[700]),
+        ),
+      ],
+    );
+  }
 }
